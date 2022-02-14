9baae40eb802ce9d80c89aef5eb328b025007fb4
tarun-code-bucket <tarun141work@gmail.com>
2022-02-14 23:16:48 +0530

    Create deply1.sh

--- scripts/deploy1.sh
+++ scripts/deploy1.sh
@@ -16,27 +16,9 @@ if [[ "parameters/"$PARAMETERS_FILE_NAME != *.properties ]]; then
     exit 0
 fi
 
-if [[ $CHANGESET_MODE == "true" ]] || [[ $CHANGESET_MODE == "True" ]]; then
-    aws cloudformation deploy \
-    --stack-name $STACK_NAME \
-    --template-file cloudformation/$TEMPLATE_NAME \
-    --parameter-overrides file://parameters/$PARAMETERS_FILE_NAME \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --region $REGION
-else
-    aws cloudformation deploy \
-    --stack-name $STACK_NAME \
-    --template-file cloudformation/$TEMPLATE_NAME \
-    --parameter-overrides file://parameters/$PARAMETERS_FILE_NAME \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --region $REGION \
-    --no-execute-changeset
-fi
-
 aws cloudformation deploy \
     --stack-name "AopsStack" \
     --template-file cloudformation/EC2-Instance.yaml\
     --capabilities CAPABILITY_IAM \
     --parameter-overrides file://parameters/dbt-deploy-stack.properties \
     --region "us-east-1"
-    --no-execute-changeset

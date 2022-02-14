6907382b556ff02134a7cb7b9da6ed8d9b2d0cc4
tarun-code-bucket <tarun141work@gmail.com>
2022-02-14 22:39:51 +0530

    Create deply1.sh

--- scripts/deploy1.sh
+++ scripts/deploy1.sh
@@ -4,16 +4,7 @@
 # Enter the name of the stack, the parameters file name, the template name, then changeset condition, and finally the region name.
 
 
-if [ $# -ne 5 ]; then
-    echo "Enter stack name, parameters file name, template file name to create, set changeset value (true or false), and enter region name. "
-    exit 0
-else
-    STACK_NAME=$1
-    PARAMETERS_FILE_NAME=$2
-    TEMPLATE_NAME=$3
-    CHANGESET_MODE=$4
-    REGION=$5
-fi
+
 
 if [[ "cloudformation/"$TEMPLATE_NAME != *.yaml ]]; then
     echo "CloudFormation template $TEMPLATE_NAME does not exist. Make sure the extension is *.yaml and not (*.yml)"
